0096e6e894728a3d61c444d22950a7b9000eb829
alexabram <alexabram@yahoo.com>
2021-09-19 11:15:29 -0700

    client: Implemented timer.

--- src/client/client.ino
+++ src/client/client.ino
@@ -21,6 +21,9 @@ const int SPEAKER_PIN = 15; // D8
 const int PUSH_BUTTON_PIN = 16; // D0
 #define SPEAKER_NOTE_FREQ 227;
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2); // LCD addr, 16 cols, 2 rows
+bool sec_reset = false;
+unsigned long timeInitial = 0;
+unsigned long timeFinal = 0;
 
 // When recv data, callback here and exec this function
 void recvCallback(uint8_t * sender_mac_addr, uint8_t *incomingMsg, uint8_t msg_len){
@@ -105,12 +108,22 @@ void alerts_off(){
   digitalWrite(LED_PIN, LOW); // LED off
 }
 
+void print_elapsed_time(){
+  timeFinal = millis()/1000;
+  unsigned long sec_elapsed = timeFinal - timeInitial;
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(sec_elapsed);
+}
+
 void loop() {
   if(msg.motion == 1){
+    timeInitial = millis()/1000;
     play_note();
     digitalWrite(LED_PIN, HIGH); // LED on
     check_push_button(); // UNCOMMENT IF USING PUSH_BUTTON - elapses 12 seconds
     alerts_off();
   }
+  print_elapsed_time();
   msg.motion = 0;
 }
